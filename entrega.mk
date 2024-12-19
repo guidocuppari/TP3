437dfce425bd5fd953fb1a53f543d49aa8eb1f10
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2024-12-19 17:14:41 -0300

    actualizado

--- entrega.mk
+++ entrega.mk
@@ -1,4 +1,5 @@
-recomendify: recomendify.py grafo.py biblioteca.py heap.py pila.py cola.py infomacion.py
+recomendify: recomendify.py grafo.py biblioteca.py heap.py pila.py cola.py funciones_auxiliares.py estructuras.py 
+rango.py recomendacion.py ciclo.py mas_importantes.py camino.py
 	rm -f ./recomendify
 	cp recomendify.py recomendify
 	chmod +x recomendify
